c3a055f6fcd352b5258d94c03dea311ee32077f3
williamtang <tangweilin2087@gmail.com>
2017-01-11 21:52:03 +0800

    add AlertDialog and ProgressDialog controls

--- HelloWorld/app/src/main/java/com/learning/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/learning/helloworld/HelloWorldActivity.java
@@ -1,5 +1,6 @@
 package com.learning.helloworld;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,7 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 public class HelloWorldActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,18 +32,47 @@ public class HelloWorldActivity extends AppCompatActivity implements View.OnClic
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-                String inputText = editText.getText().toString();
-                Toast.makeText(HelloWorldActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                /*String inputText = editText.getText().toString();
+                Toast.makeText(HelloWorldActivity.this, inputText, Toast.LENGTH_SHORT).show();*/
 
                 // show another image
-                imageView.setImageResource(R.drawable.img_2);
+                //imageView.setImageResource(R.drawable.img_2);
 
                 // progress bar
+                /*
                 if (progressBar.getVisibility() == View.GONE) {
                     progressBar.setVisibility(View.VISIBLE);
                 } else {
                     progressBar.setVisibility(View.GONE);
-                }
+                } */
+
+                /*int progress = progressBar.getProgress();
+                progress = progress + 10;
+                progressBar.setProgress(progress);*/
+
+                // alert dialog
+                /*AlertDialog.Builder dialog = new AlertDialog.Builder(HelloWorldActivity.this);
+                dialog.setTitle("This is Dialog");
+                dialog.setMessage("Something import.");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                dialog.show();*/
+
+                // progress dialog
+                ProgressDialog progressDialog = new ProgressDialog(HelloWorldActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
                 break;
             default:
                 break;
