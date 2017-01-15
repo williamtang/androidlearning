f56617d03839fa1f38c94d37efb8c50b5bfaaf72
williamtang <tangweilin2087@gmail.com>
2017-01-16 00:37:49 +0800

    ListView control

---
+++ ListViewTest/app/src/main/java/com/learning/listviewtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.learning.listviewtest;
+
+/**
+ * Created by tangweilin on 2017/1/15.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
---
+++ ListViewTest/app/src/main/java/com/learning/listviewtest/FruitAdapter.java
@@ -0,0 +1,40 @@
+package com.learning.listviewtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by tangweilin on 2017/1/15.
+ */
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+    private int resourceId;
+    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        /*View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);*/
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+        } else {
+            view = convertView;
+        }
+        ImageView fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView)view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+        return view;
+    }
+}
